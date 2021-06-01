30e0f02d084f7d040724b2a38d72bf37d475df75
Andylinee <andylin0404@gmail.com>
2021-06-02 00:51:01 +0800

    Add top_token_list function

--- CreatePriceTable.py
+++ CreatePriceTable.py
@@ -34,3 +34,32 @@ class CreatePriceTable:
                     tp.close()
                 jf.close()
         return FlexMessage
+
+    def top_token_list(sentence):
+        num_top = sentence.split(' ')[1]
+        json_file = "FlexMessage.json"
+        token_file = "TokenPrice.json"
+        with open(json_file, 'r') as jf:
+            FlexMessage = json.load(jf)
+            #print(FlexMessage)
+            request_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=" + num_top + "&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d"
+            response = requests.get(request_url)
+            data = json.loads(response.text)
+            for i in range(int(num_top)):
+                with open(token_file, 'r') as tp:
+                    print(token)
+                    token_json = json.load(tp)
+                    print(token_json)
+                    token_json["contents"][0]["text"] = data[i]['symbol']
+                    token_json["contents"][1]["text"] = str(data[0]['current_price'])
+                    token_json["contents"][2]["text"] = "{0:.2f}%".format(data[0]['price_change_percentage_24h_in_currency'])
+                    try:
+                        token_json["contents"][3]["text"] = "{0:.2f}%".format(data[0]['price_change_percentage_7d_in_currency'])
+                    except:
+                        token_json["contents"][3]["text"] = "{0:.2f}%".format(data[0]['price_change_percentage_7d_in_currency'])
+                    FlexMessage["body"]["contents"][0]["contents"].append(token_json)
+                    print("-" * 30)
+                    print(token_json)
+                    tp.close()
+                jf.close()
+        return FlexMessage
--- main.py
+++ main.py
@@ -39,11 +39,16 @@ def handle_message(event):
     message = event.message.text
     sent = message.split(' ')
     if sent[0] == '$':
-        flex_message = CreatePriceTable.create_price_table(message)
-        FlexMessage = flex_message
-    #print(FlexMessage, type(FlexMessage))
-    #FlexMessage = json.load(open('FlexMessage.json', 'r', encoding='utf-8'))
-    line_bot_api.reply_message(reply_token, FlexSendMessage('Price Table', FlexMessage))
+        if sent[1] == "top":
+            flex_message = CreatePriceTable.top_token_list(message)
+            FlexMessage = flex_message
+            line_bot_api.reply_message(reply_token, FlexSendMessage('Top Token List', FlexMessage))
+        else:
+            flex_message = CreatePriceTable.create_price_table(message)
+            FlexMessage = flex_message
+            #print(FlexMessage, type(FlexMessage))
+            line_bot_api.reply_message(reply_token, FlexSendMessage('Price Table', FlexMessage))
+    
     #line_bot_api.reply_message(reply_token, TextSendMessage(text = event.message.text))
 
 if __name__ == "__main__":
